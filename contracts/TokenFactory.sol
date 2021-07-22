bfb949ee099072b158fb1759b662ab9a1d5efe7b
MASDXI <sirawitt42@gmail.com>
2021-07-22 13:34:06 +0700

    Adding file and refactor TokenFactory

--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
-import "./Token.sol";
+import "./TokenERC20.sol";
+import "./TokenERC721.sol";
+import "./TokenERC777.sol";
+import "./TokenERC1155.sol";
 
 contract Factory {
     
@@ -38,74 +41,41 @@ contract Factory {
     }
 
     /** 
-     * @param _name name
-     * @param _symbol symbol
-     * @param _baseURI baseURI
      * @dev to create token require following parameters
      * @return address after token created successful
     */
-    function createERC721(
-        string memory _name, 
-        string memory _symbol, 
-        string memory _baseURI
-    ) public returns (address, uint256) {
-        _token Token = new _token(_name, _symbol, _baseURI);
-        erc721address.push(address(Token));
-        emit TokenCreated(address(Token));
-        return (address(Token),erc721address.length);
-    }
-
-    /** 
-     * @param _name name
-     * @param _symbol symbol
-     * @param _decimals decimals
-     * @param _totalSupply totalSupply
-     * @param _defaultOperators defaultOperators
-     * @param _tokenType type of token
-     * @dev to create token require following parameters
-     * @return address after token created successful
-    */
-    function createERC777(
-        string memory _name, 
-        string memory _symbol, 
-        uint8 _decimals,
-        uint256 _totalSupply,
-        address [] memory _defaultOperators,
-        _token.TOKEN_TYPE _tokenType
-    ) public returns (address, uint256) {
-        _token Token = new _token(_name, 
-            _symbol, 
-            _decimals, 
-            _totalSupply, 
-            _defaultOperators, 
-            _tokenType);
+    function createERC721() public returns (address, uint256) {
+        _token Token = new _token();
         erc777address.push(address(Token));
         emit TokenCreated(address(Token));
         return (address(Token),erc777address.length);
     }
 
     /** 
-     * @param _name name
-     * @param _symbol symbol
-     * @param _decimals decimals
-     * @param _totalSupply totalSupply
-     * @param _defaultOperators defaultOperators
-     * @param _tokenType type of token
      * @dev to create token require following parameters
      * @return address after token created successful
     */
-    function createERC1155(
-        string memory _uri, 
-        uint256 [] _totalSupply
-    ) public returns (address, uint256) {
-        _token Token = new _token(_uri, _totalSupply);
+    function createERC777() public returns (address, uint256) {
+        _token Token = new _token();
+        erc777address.push(address(Token));
+        emit TokenCreated(address(Token));
+        return (address(Token),erc777address.length);
+    }
+
+    /** 
+     * @dev to create token require following parameters
+     * @return address after token created successful
+    */
+    function createERC1155() public returns (address, uint256) {
+        _token Token = new _token();
         erc1155address.push(address(Token));
         emit TokenCreated(address(Token));
         return (address(Token),erc1155address.length);
     }
 
     
-    // helper function for retreive address easier
+    // helper function for retreive address easier  
+    
     // function getTokenAddress(uint256 _index) public view returns (address){
     //     return tokenAddress[_index];
     // }
