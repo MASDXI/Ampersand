f36706dfa59daee495369b5cb64a5cb9c46db071
MASDXI <sirawitt42@gmail.com>
2021-09-08 09:22:25 +0700

    Update test case

--- test/erc721.test.js
+++ test/erc721.test.js
@@ -1,19 +1,45 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("ERC721", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("ERC721 non fungible token", function () {
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+  let token;
+  let accounts;
+  const constructor = {
+    _name: "test",
+    _symbol: "TKN",
+    _initialSupply: 1000000,
+  }
+  const amount = ethers.utils.parseEther("1")
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+  before(async () => {
+    const contract = await ethers.getContractFactory("_ERC721");
+    token = await contract.deploy(
+      constructor._name,
+      constructor._symbol,
+    );
+    accounts = await ethers.getSigners();
+    await token.deployed();
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
+
+  it("Token name check", async function () {
+    expect(await token.name()).to.equal(constructor._name);
+  });
+
+  it("Token symbol check", async function () {
+    expect(await token.symbol()).to.equal(constructor._symbol);
+  });
+
+  // it("Assigns initial balance", async function() {
+  //   expect(await token.balanceOf(accounts[0].address)).to.equal(
+  //     ethers.utils.parseEther(
+  //       constructor._initialSupply.toString()
+  //     ));
+  // });
+  
+  // it("capped token mint function should be reverted", async function() {
+  //  await expect(token.mint(accounts[0].address,amount)).to.be.revertedWith('support only uncapped token');
+  // });
+
 });
