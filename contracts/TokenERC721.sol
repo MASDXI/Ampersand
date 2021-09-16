2cecc3d5ac0485a8b69a77a49ee40922143a123b
MASDXI <sirawitt42@gmail.com>
2021-09-16 18:06:10 +0700

    Update modifier transferable ERC721

--- contracts/TokenERC721.sol
+++ contracts/TokenERC721.sol
@@ -9,12 +9,22 @@ contract _ERC721 is ERC721URIStorage, Ownable {
 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
+    enum TOKEN_TYPE {TRANSFERABLE,UNTRANFERABLE}
+    bool private royalty
+    TOKEN_TYPE public tokenType;
 
     constructor(
         string memory _name,
-        string memory _symbol)
+        string memory _symbol,
+        bool _royalty
+        )
         ERC721(_name, _symbol) {
         transferOwnership(tx.origin);
+    }
+
+    modifier transferable {
+      require(tokenType == _ERC721.TOKEN_TYPE.TRANSFERABLE,"support only transferable token");
+      _;
     }
 
     function mint(address _address, string memory _tokenURI) public returns (uint256) {
@@ -24,4 +34,17 @@ contract _ERC721 is ERC721URIStorage, Ownable {
         _setTokenURI(newTokenId, _tokenURI);
         return newTokenId;
     }
+
+    function Transfer(address _address, address _to, uint256 _tokenId) public transferable return (bool) {
+        _transfer(_address,_to,_tokenId);
+        return true
+    }
+
+    // This function with implementing when needed usually on marketplace platform
+    // function _beforeTokenTransfer(address from,address to,uint256 tokenId) internal virtual override (ERC721, ERC721Pausable) {
+    //     super._beforeTokenTransfer(from, to, tokenId);
+    //     // do stuff or something before every transfer
+    //     // e.g. royalty fees
+    // }
+
 }
