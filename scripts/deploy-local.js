0134bf0b11181edef81b0e97bc255369dfbaefb1
MASDXI <sirawitt42@gmail.com>
2021-09-30 16:36:08 +0700

    Update deploy-local script

---
+++ scripts/deploy-local.js
@@ -0,0 +1,21 @@
+const hre = require("hardhat");
+
+async function main() {
+  const contract = await hre.ethers.getContractFactory("FactoryClone");
+  const factory = await contract.deploy();
+
+  await factory.deployed();
+
+  console.log("TokenFactory address:", factory.address);
+  const createToken = await factory.createToken('Adisak', 'SSSSS', 1);
+  const receipt = await createToken.wait();
+  const output = receipt.events[0];
+  console.log("createToken address:",output.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
