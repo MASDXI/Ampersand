e67ffe28ae3e6d4ccd3e1a18c264baed35414c06
MASDXI <sirawitt42@gmail.com>
2021-10-19 14:19:00 +0700

    update deploy script

--- scripts/deploy-local.js
+++ scripts/deploy-local.js
@@ -1,19 +1,18 @@
 const hre = require("hardhat");
-const fs = require('fs');
+const fs = require("fs");
 
 async function main() {
-
   const contract = await hre.ethers.getContractFactory("FactoryClone");
   const FactoryClone = await contract.deploy();
   await FactoryClone.deployed();
   // return contract address
   console.log("Factory Clone deployed to:", FactoryClone.address);
   // write address to file
-  let obj = `{\n\  "FactoryAddress\": "${FactoryClone.address}"\n}`
+  const obj = `{\n\  "FactoryAddress\": "${FactoryClone.address}"\n}`;
   try {
-    const data = fs.writeFileSync('artifacts/deployed.json', obj)
-    } catch (err) {
-      console.error(err)
+    const data = fs.writeFileSync("artifacts/deployed.json", obj);
+  } catch (err) {
+    console.error(err);
   }
 }
 
--- scripts/deploy-testnet.js
+++ scripts/deploy-testnet.js
@@ -1,5 +1,5 @@
 const hre = require("hardhat");
-const fs = require('fs');
+const fs = require("fs");
 
 async function main() {
   const contract = await hre.ethers.getContractFactory("FactoryClone");
@@ -8,11 +8,11 @@ async function main() {
   // return contract address
   console.log("Factory Clone deployed to:", FactoryClone.address);
   // write address to file
-  let obj = `{\n\  "FactoryAddress\": "${FactoryClone.address}"\n}`
+  const obj = `{\n\  "FactoryAddress\": "${FactoryClone.address}"\n}`;
   try {
-    const data = fs.writeFileSync('artifacts/deployed.json', obj)
-    } catch (err) {
-      console.error(err)
+    const data = fs.writeFileSync("artifacts/deployed.json", obj);
+  } catch (err) {
+    console.error(err);
   }
 }
 
